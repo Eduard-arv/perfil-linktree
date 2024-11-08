f7e2d38c2d7791d8180abc4a2b87828db8f950be
Eduard-arv <eduard62691@gmail.com>
2024-11-08 18:34:23 -0500

    actualizacion2 proyecto 101

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+ // Nombre del perfil en una constante
+ const nombrePerfil = "Eduard Rivas";
+ // Descripción del perfil en una constante
+ const descripcionPerfil = "Estudiante en Desarrollo de Sotware 💻";
 
+ // Obtenemos el elemento HTML con el ID "descripcion" (este elemento debe existir en tu HTML)
+ const descripcion = document.getElementById("descripcion");
+
+ // Creamos un nuevo elemento HTML de tipo "h1" (encabezado)
+ const nameElement = document.createElement("h1");
+ // Asignamos el nombre del perfil al contenido del encabezado
+ nameElement.textContent = nombrePerfil;
+
+ // Creamos un nuevo elemento HTML de tipo "p" (párrafo)
+ const descriptionElement = document.createElement("p");
+ // Asignamos la descripción del perfil al contenido del párrafo
+ descriptionElement.textContent = descripcionPerfil;
+
+ // Agregamos el encabezado (nombre) como hijo del elemento "profile-info"
+ profileInfo.appendChild(nameElement);
+ // Agregamos el párrafo (descripción) como hijo del elemento "profile-info"
+ profileInfo.appendChild(descriptionElement);
